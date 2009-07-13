ab2d81e49a1ae3a76554883310e51a6fd440fce6
Andy Lester <andy@petdance.com>
2009-07-12 23:47:03 -0500

    Moved Test::Pod::_parser to the end of the file

--- Pod.pm
+++ Pod.pm
@@ -1,43 +1,3 @@
-package Test::Pod::_parser;
-use base 'Pod::Simple';
-use strict;
-
-sub _handle_element_start {
-    my($parser, $element_name, $attr_hash_r) = @_;
-
-    # Curiously, Pod::Simple supports L<text|scheme:...> rather well.
-
-    if( $element_name eq "L" and $attr_hash_r->{type} eq "url") {
-        $parser->{_state_of_concern}{'Lurl'} = $attr_hash_r->{to};
-    }
-
-    return $parser->SUPER::_handle_element_start(@_);
-}
-
-sub _handle_element_end {
-    my($parser, $element_name) = @_;
-
-    delete $parser->{_state_of_concern}{'Lurl'}
-        if $element_name eq "L" and exists $parser->{_state_of_concern}{'Lurl'};
-
-    return $parser->SUPER::_handle_element_end(@_);
-}
-
-sub _handle_text {
-    my($parser, $text) = @_;
-    if( my $href = $parser->{_state_of_concern}{'Lurl'} ) {
-        if( $href ne $text ) {
-            my $line = $parser->line_count() -2; # XXX: -2, WHY WHY WHY??
-
-            $parser->whine($line, "L<text|scheme:...> is invalid according to perlpod");
-        }
-    }
-
-    return $parser->SUPER::_handle_text(@_);
-}
-
-1;
-
 package Test::Pod;
 
 use strict;
@@ -330,3 +290,44 @@ as the Artistic License v2.0 or GNU Public License v2.0.
 =cut
 
 1;
+
+package Test::Pod::_parser;
+use base 'Pod::Simple';
+use strict;
+
+sub _handle_element_start {
+    my($parser, $element_name, $attr_hash_r) = @_;
+
+    # Curiously, Pod::Simple supports L<text|scheme:...> rather well.
+
+    if( $element_name eq "L" and $attr_hash_r->{type} eq "url") {
+        $parser->{_state_of_concern}{'Lurl'} = $attr_hash_r->{to};
+    }
+
+    return $parser->SUPER::_handle_element_start(@_);
+}
+
+sub _handle_element_end {
+    my($parser, $element_name) = @_;
+
+    delete $parser->{_state_of_concern}{'Lurl'}
+        if $element_name eq "L" and exists $parser->{_state_of_concern}{'Lurl'};
+
+    return $parser->SUPER::_handle_element_end(@_);
+}
+
+sub _handle_text {
+    my($parser, $text) = @_;
+    if( my $href = $parser->{_state_of_concern}{'Lurl'} ) {
+        if( $href ne $text ) {
+            my $line = $parser->line_count() -2; # XXX: -2, WHY WHY WHY??
+
+            $parser->whine($line, "L<text|scheme:...> is invalid according to perlpod");
+        }
+    }
+
+    return $parser->SUPER::_handle_text(@_);
+}
+
+1;
+
